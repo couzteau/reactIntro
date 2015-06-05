35327014296256eb702d933016c0670304909a71
couzteau <jhagenst@adobe.com>
2015-06-05 16:33:48 -0700

    Step one/ just rename to jsx

--- js/views/app-view.jsx
+++ js/views/app-view.jsx
@@ -1,111 +1,43 @@
-/*global React, Backbone, jQuery, _, ENTER_KEY, document */
-
+/*global Backbone, jQuery, _, ENTER_KEY */
 var app = app || {};
 
-(function () {
-	'use strict';
-  
-  var StatsComponent = React.createClass({
-      clearCompleted: function () {
-        _.invoke(this.props.todos.completed(), 'destroy');
-        return false;
-      },
-    
-    render: function () {
-      var completed = this.props.todos.completed().length,
-          remaining = this.props.todos.remaining().length,
-          itemLabel = remaining === 1 ? 'item' : 'items',
-          clearButton = !completed ? null : (<button onClick={this.clearCompleted} id="clear-completed">Clear completed ({completed})</button>),
-          allFilterClass = app.TodoFilter === "" ? "selected" : "",
-          activeFilterClass = app.TodoFilter === "active" ? "selected" : "",
-          completedFilterClass = app.TodoFilter === "completed" ? "selected" : "";
-            
-      return (<div>
-          <span id="todo-count"><strong>{remaining}</strong> {itemLabel} left</span>
-          <ul id="filters">
-            <li>
-            <a className={allFilterClass} href="#/">All</a>
-            </li>
-            <li>
-              <a className={activeFilterClass} href="#/active">Active</a>
-            </li>
-            <li>
-              <a className={completedFilterClass} href="#/completed">Completed</a>
-            </li>
-          </ul>
-          {clearButton}
-        </div>);
-    }
-  });
-  
-  var AppComponent = React.createClass({
-    handleKeyPress: function (e) {
-      var input = this.refs.newTodo.getDOMNode(),
-          text = input.value.trim();
-      
-      if (e.which === ENTER_KEY && text) {
-        this.props.todos.create({
-          title: text,
-          order: this.props.todos.nextOrder(),
-          completed: false
-        });
-        input.value = "";
-      }
-    },
-    
-    allComplete: function () {
-        return this.props.todos.remaining().length === 0;
-    },
-    
-    toggleAllComplete: function () {
-        var completed = !this.allComplete();
+(function ($) {
+  'use strict';
 
-        this.props.todos.each(function (todo) {
-            todo.save({
-                completed: completed
-            });
-        });
-    },
-    
-    render: function () {
-      var stats = !this.props.todos.length ? null : (<StatsComponent todos={this.props.todos} />),
-          allComplete = this.allComplete(),
-          todoItems = this.props.todos.filter(function (todo) {
-            return todo.get("completed") ? app.TodoFilter !== "active" : app.TodoFilter !== "completed";
-          }).map(function (todo) {
-            return (<app.ItemComponent todo={todo} key={todo.get("order")} />);
-          });
-          
-      var main = null;
-      if (this.props.todos.length) {
-        main = (<section id="main">
-          <input id="toggle-all" type="checkbox" onChange={this.toggleAllComplete} checked={allComplete} />
-          <label htmlFor="toggle-all">Mark all as complete</label>
-          <ul id="todo-list">{todoItems}</ul>
-        </section>);
-      }
-      
-      return (<div>
-        <header id="header">
-          <h1>todos</h1>
-          <input id="new-todo" placeholder="What needs to be done?" autoFocus onKeyPress={this.handleKeyPress} ref="newTodo" />
-        </header>
-        {main}
-        <footer id="footer">{stats}</footer>
-        </div>
-      );
-    }
-  });
-  
   // The Application
   // ---------------
 
   // Our overall **AppView** is the top-level piece of UI.
   app.AppView = Backbone.View.extend({
+
+    // Instead of generating a new element, bind to the existing skeleton of
+    // the App already present in the HTML.
+    el: '#todoapp',
+
+    // Our template for the line of statistics at the bottom of the app.
+    statsTemplate: _.template($('#stats-template').html()),
+
+    // Delegated events for creating new items, and clearing completed ones.
+    events: {
+      'keypress #new-todo': 'createOnEnter',
+      'click #clear-completed': 'clearCompleted',
+      'click #toggle-all': 'toggleAllComplete'
+    },
+
+    // At initialization we bind to the relevant events on the `Todos`
+    // collection, when items are added or changed. Kick things off by
+    // loading any preexisting todos that might be saved in *localStorage*.
     initialize: function () {
-      React.render(<AppComponent todos={app.todos} />, document.getElementById("todoapp"));
-      this.listenTo(app.todos, 'change:completed', this.render);
-      this.listenTo(app.todos, 'filter', this.render);
+      this.allCheckbox = this.$('#toggle-all')[0];
+      this.$input = this.$('#new-todo');
+      this.$footer = this.$('#footer');
+      this.$main = this.$('#main');
+      this.$list = $('#todo-list');
+
+      this.listenTo(app.todos, 'add', this.addOne);
+      this.listenTo(app.todos, 'reset', this.addAll);
+      this.listenTo(app.todos, 'change:completed', this.filterOne);
+      this.listenTo(app.todos, 'filter', this.filterAll);
       this.listenTo(app.todos, 'all', this.render);
 
       // Suppresses 'add' events with {reset: true} and prevents the app view
@@ -117,8 +49,83 @@ var app = app || {};
     // Re-rendering the App just means refreshing the statistics -- the rest
     // of the app doesn't change.
     render: function () {
-      React.render(<AppComponent todos={app.todos} />, document.getElementById("todoapp"));
+      var completed = app.todos.completed().length;
+      var remaining = app.todos.remaining().length;
+
+      if (app.todos.length) {
+        this.$main.show();
+        this.$footer.show();
+
+        this.$footer.html(this.statsTemplate({
+          completed: completed,
+          remaining: remaining
+        }));
+
+        this.$('#filters li a')
+          .removeClass('selected')
+          .filter('[href="#/' + (app.TodoFilter || '') + '"]')
+          .addClass('selected');
+      } else {
+        this.$main.hide();
+        this.$footer.hide();
+      }
+
+      this.allCheckbox.checked = !remaining;
+    },
+
+    // Add a single todo item to the list by creating a view for it, and
+    // appending its element to the `<ul>`.
+    addOne: function (todo) {
+      var view = new app.TodoView({ model: todo });
+      this.$list.append(view.render().el);
+    },
+
+    // Add all items in the **Todos** collection at once.
+    addAll: function () {
+      this.$list.html('');
+      app.todos.each(this.addOne, this);
+    },
+
+    filterOne: function (todo) {
+      todo.trigger('visible');
+    },
+
+    filterAll: function () {
+      app.todos.each(this.filterOne, this);
+    },
+
+    // Generate the attributes for a new Todo item.
+    newAttributes: function () {
+      return {
+        title: this.$input.val().trim(),
+        order: app.todos.nextOrder(),
+        completed: false
+      };
+    },
+
+    // If you hit return in the main input field, create new **Todo** model,
+    // persisting it to *localStorage*.
+    createOnEnter: function (e) {
+      if (e.which === ENTER_KEY && this.$input.val().trim()) {
+        app.todos.create(this.newAttributes());
+        this.$input.val('');
+      }
+    },
+
+    // Clear all completed todo items, destroying their models.
+    clearCompleted: function () {
+      _.invoke(app.todos.completed(), 'destroy');
+      return false;
+    },
+
+    toggleAllComplete: function () {
+      var completed = this.allCheckbox.checked;
+
+      app.todos.each(function (todo) {
+        todo.save({
+          completed: completed
+        });
+      });
     }
   });
-
-})();
+})(jQuery);
