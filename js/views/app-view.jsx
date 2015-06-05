960e8b3daddc383b05515259f3849c71ac4b574a
couzteau <jhagenst@adobe.com>
2015-06-05 16:38:36 -0700

    move template out of HTML section and add make the app-view module a React component that spits out the HTML cut out from the section

--- js/views/app-view.jsx
+++ js/views/app-view.jsx
@@ -128,4 +128,23 @@ var app = app || {};
       });
     }
   });
+  
+  var AppComponent = React.createClass({
+    render: function () {
+      return (<div>
+        <header id="header">
+          <h1>todos</h1>
+          <input id="new-todo" placeholder="What needs to be done?" autofocus />
+        </header>
+        <section id="main">
+          <input id="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul id="todo-list"></ul>
+        </section>
+        <footer id="footer"></footer>
+        </div>
+      );
+    }
+  });  
+  
 })(jQuery);
