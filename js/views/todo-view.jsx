18cd93907d9c540b9d085a1c5591d43717c1418c
couzteau <jhagenst@adobe.com>
2015-06-09 16:07:42 -0700

    todos are now editable, updatable and closable.

--- js/views/todo-view.jsx
+++ js/views/todo-view.jsx
@@ -10,21 +10,89 @@ var app = app || {};
     // Toggle the `"completed"` state of the model.
     toggleCompleted: function () {
       this.model.toggle();
+    },
+    
+    getInitialState: function () {
+      return {
+        editing: false
+      };
+    },
+    
+    edit: function (e) {
+        this.setState({
+            editing: true
+        });
+    },
+
+    componentDidUpdate: function (oldProps, oldState) {
+        if (this.state.editing && !oldState.editing) {
+            this.refs.editField.getDOMNode().focus();
+        }
+    },
+    
+    close: function () {
+        var value = this.refs.editField.getDOMNode().value;
+			  var trimmedValue = value.trim();
+
+		 	  if (!this.state.editing) {
+            return;
+			  }
+
+			  if (trimmedValue) {
+				    this.props.todo.save({ title: trimmedValue });
+
+            if (value !== trimmedValue) {
+                // Model values changes consisting of whitespaces only are
+                // not causing change to be triggered Therefore we've to
+                // compare untrimmed version with a trimmed one to check
+                // whether anything changed
+                // And if yes, we've to trigger change event ourselves
+                this.props.todo.trigger('change');
+            }
+			  } else {
+            this.clear();
+			  }
+
+			  this.setState({
+                editing: false
+              });
+		},
+
+		// If you hit `enter`, we're through editing the item.
+		updateOnEnter: function (e) {
+        if (e.which === ENTER_KEY) {
+            this.close();
+        }
+	},
+    
+    clear: function () {
+      this.props.todo.destroy();
     },    
     
+    revertOnEscape: function (e) {
+        if (e.which === ESC_KEY) {
+            this.setState({
+                editing: false
+            });
+            // Also reset the hidden input back to the original value.
+            this.refs.editField.getDOMNode().value = this.props.todo.get('title');
+        }
+    },    
+
     render: function () {
       var todo = this.props.todo,
           itemClasses = React.addons.classSet({
-            completed: todo.get("completed")
+            completed: todo.get("completed"),
+            editing: this.state.editing
           });
       
       return (<li className={itemClasses}>
         <div className="view">
           <input className="toggle" type="checkbox" onChange={this.toggleCompleted} checked={this.props.todo.get("completed")} />    
-          <label>{todo.get("title")}</label>
+          <label onDoubleClick={this.edit}>{todo.get("title")}</label>
           <button className="destroy"></button>
         </div>
-        <input className="edit" defaultValue={todo.get("title")} />
+        <input className="edit" ref="editField" onKeyDown={this.revertOnEscape} onBlur={this.close} onKeyPress={this.updateOnEnter} defaultValue={todo.get("title")} />
       </li>);
     }
   });  
